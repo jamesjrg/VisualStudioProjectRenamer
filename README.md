adc11d740922e4a9f19f9cbf5c587a69408e515c
James Gregory <james.jrg@gmail.com>
2014-12-05 17:37:28 +0000

    a bunch of work

--- README.md
+++ README.md
@@ -4,13 +4,34 @@ VisualStudioProjectRenamer
 A script to rename a C# Visual Studio project. It does the following:
 
     * Renames the project folder
+    
     * Renames the .csproj
+    
     * Alters the referenced csproj in the root .sln file
+    
     * Alters RootNamespace and AssemblyName in the csproj
+    
     * Alters the AssemblyInfo.cs file in the project, if present
-    * Alters namespace xxx lines in .cs files within the project
-    * Alters using xxx lines in all .cs files in all projects within the directory tree - i.e. if you have multiple projects within the solution, they too will have using statements altered
+    
+    * Alters namespace xxx lines in files matching a given pattern, within the project. It will make some attempt not to alter comments that happen to use the phrase "namespace", but it's still a simple regex and could fail.
+    
+    * Alters using xxx lines in files matching a given pattern, in all projects within the directory tree - i.e. if you have multiple projects within the solution, they too will have using statements altered. It will make some attempt not to alter comments that happen to use the phrase "using", but it's still a simple regex and could fail.
+    
     * Alters ProjectReference sections in other .csprojs within the directory tree, i.e. alters references to the project within other projects
+
+Notes:
+
+    * By default the file matching pattern is "*.cs". You can alter this by changing the fileTypesToMatchForReplacement array at the top of the script.
+    
+    * No backup is created, because I assume anyone using this is already using source control and can easily revert the changes if it goes wrong.
+    
+    * It assumes that you will run it in the solution folder, and that the project to be renamed is in a subfolder with the same name as the project
+    
+    * It assumes there is only a single .sln file in the directory where the script is run
+    
+If it doesn't do quite what you want then just open the script, fiddle with it to your liking, and run it again.
+
+How to run it:
 
 As long as you have a relatively recent version of Visual Studio installed you can probably have the F# interpreter for running interactive scripts installed somewhere like this:
 
@@ -24,6 +45,4 @@ or if fsi.exe is already in your PATH:
 
 fsi.exe -- <old_project_name> <new_project_name>
 
-No backup is created, because I assume anyone using this is already using source control and can easily revert the changes if it goes wrong.
 
-If it doesn't do exactly what you want then just open the script, fiddle with it to your liking, and run it again. It uses regexes rather than properly parsing XML and C#, because it is simple and it works.
--- VisualStudioProjectRenamer.fsx
+++ VisualStudioProjectRenamer.fsx
@@ -1,54 +1,99 @@
 open System.IO
 
+//Config
+
+let fileTypesToMatchForReplacement = [|"*.cs"|]
+
+//Definitions inspired by on Scott Wlaschin's railway oriented programming http://fsharpforfunandprofit.com/posts/recipe-part2/
+
+type Result = 
+    | Success of string * string
+    | Failure of string
+
+let bind func result = 
+    match result with
+    | Success (oldName, newName) -> func oldName newName
+    | Failure f -> Failure f
+
+//Interpreting command line arguments
+
 let parseArgs =
     let args = fsi.CommandLineArgs
-    if args.Length > 1 then Some (args.[0], args.[1]) else None
+    if args.Length > 1 then Success (args.[0], args.[1]) else Failure "couldn't parse command line arguments"
 
-let validateArgs oldName newName : string option =
-     match Directory.Exists oldName with
-     | true ->
+let validateArgs oldName newName =
+    match Directory.Exists oldName with
+    | true ->
         match Directory.Exists(newName) with
-        | true -> Some (sprintf "Directory already exists: %s" newName)
-        | false -> None 
-     | false -> Some (sprintf "No such directory: %s" oldName)
+        | true -> Failure (sprintf "Directory already exists: %s" newName)
+        | false -> Success (oldName, newName)
+    | false -> Failure (sprintf "No such directory: %s" oldName)
+
+// Error handler
+
+let handleError result = 
+    match result with
+    | Failure errorMsg -> printfn "%s" errorMsg
+    | _ -> ()
+
+// renaming files and directories
 
 let renameDirectory oldName newName =
-    ()
+    try
+        Directory.Move(oldName, newName)
+        Success (oldName, newName)
+    with
+        | ex -> Failure ex.Message
 
 let renameCsProj oldName newName =
-    ()
+    try
+        File.Move(oldName + ".csproj", newName + ".csproj");
+        Success (oldName, newName)
+    with
+        | ex -> Failure ex.Message
+
+// modifying files
+
+let rec getFilesForModification basePath =
+    seq {
+        for matchPattern in fileTypesToMatchForReplacement do
+            yield! Directory.EnumerateFiles(basePath, matchPattern, SearchOption.AllDirectories)
+    }
 
 let alterSlnFile oldName newName =
-    ()
+    let maybeSlnFile = Directory.EnumerateFiles(".", "*.sln") |> Seq.tryPick Some
+    match maybeSlnFile with
+    | Some slnFile -> 
+        let oldSlnContents = File.ReadAllText(slnFile) 
+        let newSlnFileContents = oldSlnContents.Replace(oldName + ".csproj", newName + ".csproj")
+        File.WriteAllText(@"C:\Users\Public\TestFolder\WriteText.txt", newSlnFileContents)
+        Success (oldName, newName)
+    | _ -> Failure "No .sln file found"
 
 let alterCsproj oldName newName =
-    ()
+    let oldSlnContents = File.ReadAllText(newName + ".csproj") 
+    Success (oldName, newName)
 
 let alterAssemblyInfo oldName newName =
-    ()
+    Success (oldName, newName)
 
 let alterNamespaces oldName newName =
-    ()
+    Success (oldName, newName)
 
 let alterUsings oldName newName =
-    ()
+    Success (oldName, newName)
 
 let alterProjectReferences oldName newName =
-    ()
+    Success (oldName, newName)
 
-let doRename oldName newName =
-    renameDirectory oldName newName
-    renameCsProj oldName newName
-    alterSlnFile oldName newName
-    alterCsproj oldName newName
-    alterAssemblyInfo oldName newName
-    alterNamespaces oldName newName
-    alterUsings oldName newName
-    alterProjectReferences oldName newName
-
-match parseArgs with
-| Some (oldName, newName) ->
-    match validateArgs oldName newName with    
-    | Some(errorMsg) -> printfn "%s" errorMsg
-    | None -> doRename oldName newName
-| None -> ()
+parseArgs 
+|> bind validateArgs
+|> bind renameDirectory
+|> bind renameCsProj
+|> bind alterSlnFile
+|> bind alterCsproj
+|> bind alterAssemblyInfo
+|> bind alterNamespaces
+|> bind alterUsings
+|> bind alterProjectReferences
+|> handleError
